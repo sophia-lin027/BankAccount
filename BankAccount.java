f0bc57a55164d997d864485901de8818e5cc2ff4
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-10-28 15:29:14 -0400

    added deposit

--- BankAccount.java
+++ BankAccount.java
@@ -23,4 +23,14 @@ public class BankAccount {
   public void setPassword(String newPass) {
     password = newPass;
   }
+
+  public boolean deposit (double amount) {
+    if (amount > 0) {
+      balance = balance + amount;
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
 }
