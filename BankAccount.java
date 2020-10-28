fe4ea4af9a164413ce678e8bfcd8d136a3d1ed78
Sophia Lin <sophia@Sophias-Mac.fios-router.home>
2020-10-28 15:01:16 -0400

    added methods for balance, accountID

---
+++ BankAccount.java
@@ -0,0 +1,22 @@
+public class BankAccount {
+  public double balance;
+  public int accountID;
+  public String password;
+
+  public BankAccount (int accountID, String password) {
+    this.accountID = accountID;
+    this.password = password;
+  }
+
+  public String toString() {
+    return "#" + accountID + "\t$" + balance;
+  }
+
+  public double getBalance() {
+    return balance;
+  }
+
+  public int getAccountID() {
+    return accountID;
+  }
+}
